2f4dff3c01eeef380dca1d55289137ae9e7fcc88
Oswaldo Montes <ing.oswaldoms@gmail.com>
2023-11-30 15:53:27 -0500

    Add tweets paginated on profile user page.

---
+++ src/main/java/com/koombea/twitterclone/web/app/models/projections/post/MessageOnly.java
@@ -0,0 +1,10 @@
+/*
+ * @creator: Oswaldo Montes
+ * @date: November 30, 2023
+ *
+ */
+package com.koombea.twitterclone.web.app.models.projections.post;
+
+public interface MessageOnly {
+    String getMessage();
+}
--- src/main/java/com/koombea/twitterclone/web/app/repositories/PostRepository.java
+++ src/main/java/com/koombea/twitterclone/web/app/repositories/PostRepository.java
@@ -6,10 +6,14 @@
 package com.koombea.twitterclone.web.app.repositories;
 
 import com.koombea.twitterclone.web.app.models.entities.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, String> {
+public interface PostRepository extends JpaRepository<Post, String> {
     Long countPostByUserId(String userId);
+
+    <T> Page<T> findAllByUserId(String userId, Pageable pageable, Class<T> type);
 }
